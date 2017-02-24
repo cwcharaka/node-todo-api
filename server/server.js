3c2dface4c5e4bcabd169d35a0872ab11a22c463
Charaka Wijesuriya <charaka@Charakas-MBP-2.gateway>
2017-02-25 10:39:15 +1100

    Add Get /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const {ObjectId} = require('mongodb')
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -29,6 +30,22 @@ app.get('/todos', (req, res) => {
   })
 });
 
+app.get('/todos/:id',(req, res) => {
+  const id = req.params.id;
+  if (!ObjectId.isValid(id)) {
+    return res.status(404).send();
+  }
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    } else {
+      res.send({todo})
+    }
+  }).catch((e) => {
+    res.status(400).send();
+  })
+
+})
 
 app.listen(3000, () => {
   console.log('Started on port 3000');
