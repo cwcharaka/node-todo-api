f262a789176e3d6f2234756472356baf88137ad5
Charaka Wijesuriya <charaka@Charakas-MBP-2.gateway>
2017-02-25 22:12:42 +1100

    Add test route and test for PATCH /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -11,7 +11,9 @@ var todos = [{
   text: 'First test todo'
 }, {
   _id: new ObjectId(),
-  text: 'Second test todo'
+  text: 'Second test todo',
+  completed: true,
+  completedAt: 333
 }]
 
 beforeEach((done) => {
@@ -133,5 +135,41 @@ describe('DELETE /todos/:id', () => {
           done(e)
         })
       });
-  })
+  });
 });
+describe('PATCH /todos/:id', () => {
+  it('Should update the todo', (done) => {
+    var id = todos[0]._id.toHexString();
+    var text = 'Updated from test';
+    request(app)
+      .patch(`/todos/${id}`)
+      .send({
+        text,
+        completed: true
+      })
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo.text).toBe(text)
+        expect(res.body.todo.completed).toBe(true)
+        expect(res.body.todo.completedAt).toBeA('number')
+      })
+      .end(done)
+  });
+  it('Should clear the completed at when todo is not completed', (done) => {
+    var id = todos[1]._id.toHexString();
+    var text = 'Updated from test';
+    request(app)
+      .patch(`/todos/${id}`)
+      .send({
+        text,
+        completed: false
+      })
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo.text).toBe(text)
+        expect(res.body.todo.completed).toBe(false)
+        expect(res.body.todo.completedAt).notToExist
+      })
+      .end(done)
+  })
+})
