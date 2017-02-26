93590b3bab9ad64e2a3fa599bd7c1669f1408204
Charaka Wijesuriya <charaka@Charakas-MacBook-Pro-2.local>
2017-02-27 10:43:48 +1100

    Add Post users route and token method

--- server/server.js
+++ server/server.js
@@ -17,7 +17,6 @@ var port = process.env.PORT
 app.use(bodyParser.json());
 
 app.post('/todos',(req, res) => {
-  console.log(req.body);
   var todo = new Todo({
     text: req.body.text
   });
@@ -91,7 +90,21 @@ app.patch('/todos/:id',(req, res) => {
   }).catch((e) => {
     res.status(400).send();
   })
-})
+});
+
+app.post('/users', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+  var user = new User(body);
+  user.save().then(() => {
+    return user.generateAuthToken();
+  }).then((token) => {
+    res.header('x-auth', token).send(user)
+  }).catch((e) => {
+    res.status(400).send(e);
+  });
+});
+
+
 
 app.listen(port, () => {
   console.log(`Started on port ${port}`);
